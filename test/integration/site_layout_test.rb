afcf04df37b88ee0458ba065eff2a029e54eeda6
Akshay <your.akshaych2786@gmail.com>
2018-10-03 00:47:12 +0530

    After chtr 7 completion

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
    test "layout links" do
-     get root_path
+    get root_path
     assert_template 'static_pages/home'
     assert_template "a[href=?]", root_path, count: 2
-    assert_template "a]href=?]", help_path
+    assert_template "a[href=?]", help_path
     assert_template "a[href=?]", about_path
     assert_template "a[href=?]", contact_path
     assert_template "a[href=?]", signup_path
